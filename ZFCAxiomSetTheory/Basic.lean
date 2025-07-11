7fdec6b4e0de777b5d55c772ef27f653ae5e72fa
julian1c2a <julian.calderon.almendros@gmail.com>
2025-07-11 18:06:48 +0200

    Refactor la clase SetUniverse, introduciendo la definición axiomática de pertenencia y mejorando la notación para subconjuntos y conjuntos disjuntos, además de agregar teoremas relacionados con la extensionalidad y la relación de orden parcial.

--- ZFCAxiomSetTheory/Basic.lean
+++ ZFCAxiomSetTheory/Basic.lean
@@ -8,18 +8,58 @@ namespace SetUniverse
   variable {U : Type u}
 
   /-! ### Clase (sin 'class' ni 'structure') que representa pertenencia estensional entre conjuntos ### -/
-  axiom ExtSet_from_Eq {U : type u} (x y : U): x = y → (∀ (z: U), mem z x ↔ mem z y)
-  def mem {U : Type u} (x y : U) : Prop :=
-    ExtSet_from_Eq x y (by rfl)
-  class ExtSet (U : Type u) where
-    mem : U → U → Prop
-    ext : ∀ (x y : U), (∀ (z: U), mem z x ↔ mem z y) → x = y
+
+  /-! ### Introducción de la pertenencia a un conjunto como símbolo primitivo ### -/
+  -- Definición axiomática de pertenencia: x pertenece a y
+  -- Aquí, 'x' es un elemento y 'y' es un conjunto
+  -- Esta definición es intencionalmente abstracta para permitir la flexibilidad en la implementación
+  -- de conjuntos en Lean.
+  axiom mem (x y : U) : Prop
 
   /-! ### Notación estándar de la pertenecia a conjuntos ### -/
-  notation:48 lhs: 49 " ∈ " rhs:49 => ExtSet.mem rhs lhs
+  notation:50 lhs:51 " ∈ " rhs:51 => mem lhs rhs
+  notation:50 lhs:51 " ∉ " rhs:51 => ¬(lhs ∈ rhs)
+
+  /-! ### Axioma de extensionalidad de conjuntos ### -/
+  /-! ### ExtSet : x = y ↔ ∀ z, z ∈ x ↔ z ∈ y ### -/
+  axiom ExtSet (x y : U): (∀ (z: U), z ∈ x ↔ z ∈ y) → (x = y)
+
+  theorem ExtSetReverse (x y : U) :
+    (x = y) → (∀ (z: U), z ∈ x ↔ z ∈ y)
+      := by
+    intro h_eq
+    intro z
+    constructor
+    · -- Dirección ->
+      intro hz_in_x
+      rw [h_eq] at hz_in_x
+      exact hz_in_x
+    · -- Dirección <-
+      intro hz_in_y
+      rw [← h_eq] at hz_in_y
+      exact hz_in_y
+
+  theorem ExtSet_wc {x y : U} (h_x_subs_y: ∀ (z: U), z ∈ x → z ∈ y) (h_y_subs_x: ∀ (z: U), z ∈ y → z ∈ x) :
+    (x = y) := by
+    apply ExtSet
+    intro z
+    constructor
+    · -- Dirección ->
+      intro hz_in_x
+      apply h_x_subs_y
+      exact hz_in_x
+    · -- Dirección <-
+      intro hz_in_y
+      apply h_y_subs_x
+      exact hz_in_y
+
+  theorem ExtSetReverse_wc {x y : U} (h_eq: x = y) :
+    (∀ (z: U), z ∈ x ↔ z ∈ y) := by
+    apply ExtSetReverse
+    exact h_eq
 
   /-! ### Subconjunto (no estricto) ### -/
-  def subseteq {U : Type u} [ExtSet U] (x y : U) : Prop :=
+  def subseteq (x y : U) : Prop :=
     ∀ (z: U), z ∈ x → z ∈ y
 
   /-! ### Notación estándar de subconjunto y subconjunto propio ### -/
@@ -32,52 +72,117 @@ namespace SetUniverse
 
   /-! ### Teorema de igualdad de conjuntos a través de ser subconjunto uno de otro ### -/
   /-! ### EqualityOfSubset : A ⊆ B ∧ B ⊆ A ↔ A = B                                ### -/
-  theorem EqualityOfSubset {U : Type u} [ExtSet U] (x y : U) :
+  theorem EqualityOfSubset (x y : U) :
     (x ⊆ y) → (y ⊆ x) → (x = y)
-    := by
+      := by
     intro h_xy h_yx
-    apply ExtSet.ext
+    apply (ExtSet x y)
     intro z
     constructor
     · -- Dirección ->
       intro hz_in_x
-      unfold subseteq at h_xy
-      have h_xy_z : z ∈ y := h_xy z hz_in_x
-      have h_subset_xy_z : ∀ (w : U), w ∈ x → w ∈ y := h_xy
-      intro hz_in_y
-      unfold subseteq at h_yx
-      have h_yx_z : z ∈ x := h_yx z hz_in_y
-      have h_subset_yx_z : ∀ (w : U), w ∈ y → w ∈ x := h_yx
-      have hequival : z ∈ y ↔ z ∈ x := by
-        constructor
-        · exact h_subset_xy_z z hz_in_x
-        · exact h_subset_yx_z z hz_in_y
-      exact
+      apply h_xy
+      exact hz_in_x
     · -- Dirección <-
       intro hz_in_y
       apply h_yx
       exact hz_in_y
 
-  /-! ### Disjuntos ### -/
+  /-! ### Disjuntos (sin relación por '⊆') ### -/
 
-  -- Instancia explícita de PartialOrder
-  instance {U : Type u} [ExtSet U] : PartialOrder U where
-    le := (· ⊆ ·) -- le (≤) es la relación de subconjunto (⊆)
-    le_refl := by -- Prueba de reflexividad: x ⊆ x
+  /-! ### 'U' es un Orden Parcial por '⊆' ### -/
+    theorem subseteq_reflexive : ∀ (x : U), x ⊆ x := by
       intro x z h_mem
       exact h_mem
-    le_trans := by -- Prueba de transitividad: x ⊆ y → y ⊆ z → x ⊆ z
+
+    theorem subseteq_transitive : ∀ (x y z : U), x ⊆ y → y ⊆ z → x ⊆ z := by
       intro x y z h_xy h_yz
       intro w h_w_in_x
       apply h_yz
       apply h_xy
       exact h_w_in_x
-    le_antisymm := by
-      intro x y h_xy h_yx
-      apply EqualityOfSubset h_xy h_yx
 
-  -- Ahora, esta instancia debería funcionar sin problemas
-  -- instance {U : Type u} [ExtSet U] : Disjoint U where
-  --   Disjoint x y := ∀ z, z ∈ x → z ∉ y
+    theorem subseteq_antisymmetric : ∀ (x y : U), x ⊆ y → y ⊆ x → x = y := by
+      intro x y h_xy h_yx
+      apply EqualityOfSubset
+      exact h_xy
+      exact h_yx
+
+  /-! ### Definición de Conjuntos Disjuntos ### -/
+  def disjoint (x y : U) : Prop :=
+    ∀ z, z ∈ x → z ∉ y
+
+  /-! ### Notación estándar de conjuntos disjuntos ### -/
+  notation:50 lhs:51 " ⟂ " rhs:51 => disjoint lhs rhs
+
+  /-! ### Simetría de los Conjuntos Disjuntos ### -/
+  theorem disjoint_symm (x y : U) :
+    (x ⟂ y) → (y ⟂ x) := by
+    intro h_disj z h_z_in_y h_z_in_x
+    apply h_disj z h_z_in_x
+    exact h_z_in_y
+
+  /-! ### Teorema de conjuntos disjuntos (todavía sin notación estándar) ### -/
+  theorem disjoint_is_empty (x y : U) :
+    (x ⟂ y) → (∃ z : U, z ∈ x ∧ z ∈ y) → False := by
+    intro h_disj h_exists
+    cases h_exists with
+    | intro z h_z_both =>
+      apply h_disj
+      exact h_z_both.1
+      exact h_z_both.2
+
+  theorem disjoint_is_empty_wc {x y : U} (h_exists :  ∃ (z : U), z ∈ x ∧ z ∈ y) :
+    ¬(x ⟂ y) := by
+    intro h_disj
+    apply disjoint_is_empty
+    exact h_disj
+    exact h_exists
+
+  /-! ### 'U' es un Orden Parcial por '⊆' ### -/
+  instance : PartialOrder U where
+    le := subseteq
+    le_refl := subseteq_reflexive
+    le_trans := subseteq_transitive
+    le_antisymm := subseteq_antisymmetric
+
+  def isEmpty (x : U) : Prop :=
+    ∀ y, ¬(y ∈ x)
+
+  def isNonEmpty (x : U) : Prop :=
+    ∃ y, y ∈ x
+
+  def isSingleton (x : U) : Prop :=
+    ∃ y, ∀ z, z ∈ x ↔ z = y
+
+  def isPair (x : U) : Prop :=
+    ∃ y z, ∀ w, w ∈ x ↔ (w = y ∨ w = z)
+
+  def isBinIntersection (x y s: U) : Prop :=
+    ∀ z, z ∈ x ↔ (z ∈ y ∧ z ∈ s)
+
+  def isBinUnion (x y s: U) : Prop :=
+    ∀ z, z ∈ x ↔ (∃ t, t ∈ y ∧ z ∈ t) ∧ (z ∈ s)
+
+  def isBinDiff (x y s: U) : Prop :=
+    ∀ z, z ∈ x ↔ (z ∈ y ∧ ¬(z ∈ s))
+
+  def isBinSymDiff (x y s: U) : Prop :=
+    ∀ z, z ∈ x ↔ (z ∈ y ∧ z ∉ s) ∨ (z ∉ y ∧ z ∈ s)
+
+  def isUnion (x X: U) : Prop :=
+    ∀ (z : U), z ∈ X ↔ ∃ (y : U), z ∈ y ∧ y ∈ x
+
+  def isIntersection (x X: U) : Prop :=
+    ∀ (z: U), z ∈ X ↔ ∀ (y: U), y ∈ x → z ∈ y
 
 end SetUniverse
+
+export SetUniverse (
+  mem ExtSet ExtSet_wc ExtSetReverse ExtSetReverse_wc EqualityOfSubset
+  subseteq subseteq_reflexive subseteq_transitive subseteq_antisymmetric
+  disjoint disjoint_symm disjoint_is_empty disjoint_is_empty_wc
+  isEmpty isNonEmpty isSingleton isPair isBinIntersection
+  isBinUnion isBinDiff isBinSymDiff isUnion
+  isIntersection
+)
