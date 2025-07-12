a098e2c36d23eb432f0ea7b688dd90e41cb10964
julian1c2a <julian.calderon.almendros@gmail.com>
2025-07-12 20:25:51 +0200

    Agregar definiciones y teoremas para subconjuntos y subconjuntos propios, incluyendo propiedades relacionadas y notaciones estándar.

--- ZFCAxiomSetTheory/Extension.lean
+++ ZFCAxiomSetTheory/Extension.lean
@@ -50,10 +50,21 @@ namespace SetUniverse
     def subseteq (x y : U) : Prop :=
       ∀ (z: U), z ∈ x → z ∈ y --
 
-    /-! ### Notación estándar de subconjunto y subconjunto propio ### -/
+    /-! ### Notación estándar de subconjunto (no estricto) ### -/
     notation:50 lhs:51 " ⊆ " rhs:51 => subseteq lhs rhs -- [cite: 124]
 
+    /-! ### Subconjunto propio ### -/
+    /-! ### Subset : x ⊆ y ∧ x ≠ y ### -/
+    def subset (x y : U) : Prop :=
+      subseteq x y ∧ (x ≠ y) -- [cite: 124]
+
+    /-! ### Notación estándar de subconjunto propio ### -/
     notation:50 lhs:51 " ⊂ " rhs:51 => subseteq lhs rhs ∧ (lhs ≠ rhs) -- [cite: 124]
+
+    /-! ### Notación estándar de superconjunto y superconjunto propio ### -/
+    notation:50 lhs:51 " ⊇ " rhs:51 => subseteq rhs lhs   -- [cite: 124]
+
+    notation:50 lhs:51 " ⊃ " rhs:51 => subset rhs lhs -- [cite: 124]
 
     /-! ### Teorema de igualdad de conjuntos a través de ser subconjunto uno de otro ### -/
     theorem EqualityOfSubset (x y : U) :
@@ -97,6 +108,93 @@ namespace SetUniverse
     /-! ### Notación estándar de conjuntos disjuntos ### -/
     notation:50 lhs:51 " ⟂ " rhs:51 => disjoint lhs rhs -- [cite: 133]
 
+    /-! ### Propiedades de la relación de Subconjunto Propio ### -/
+    theorem subset_subseteq (x y : U) : x ⊂ y → x ⊆ y := by
+      intro h_subs
+      exact h_subs.1
+
+    theorem subseteq_subset_or_eq (x y : U) : x ⊆ y → (x ⊂ y ∨ x = y) := by
+      intro h_subs
+      by_cases h_eq : x = y
+      · -- Caso x = y
+        right
+        exact h_eq
+      · -- Caso x ≠ y
+        left
+        constructor
+        · exact h_subs
+        · exact h_eq
+
+    theorem subset_irreflexive (x : U) : ¬(x ⊂ x) := by
+      intro h_subs
+      apply h_subs.2
+      rfl
+
+    theorem subset_asymmetric (x y : U) : (x ⊂ y) → ¬(y ⊂ x) := by
+      intro h_subs
+      intro h_subs_reverse
+      apply h_subs.2
+      apply EqualityOfSubset
+      exact h_subs.1
+      exact h_subs_reverse.1
+
+    theorem subset_asymmetric' (x y : U) : (x ⊆ y) → ¬(y ⊂ x) := by
+      intro h_subs
+      by_cases h_eq : x = y
+      · -- Caso x = y
+        intro h_subs_reverse
+        apply h_subs_reverse.2
+        exact h_eq.symm
+      · -- Caso x ≠ y
+        intro h_subs_reverse
+        apply h_subs_reverse.2
+        apply EqualityOfSubset
+        exact h_subs_reverse.1
+        exact h_subs
+
+    theorem subset_transitive (x y z : U) : (x ⊂ y) → (y ⊂ z) → (x ⊂ z) := by
+      intro h_subs_xy h_subs_yz
+      constructor
+      · apply subseteq_transitive -- [cite: 136]
+        exact h_subs_xy.1
+        exact h_subs_yz.1
+      · intro h_eq
+        apply h_subs_xy.2
+        apply EqualityOfSubset
+        exact h_subs_xy.1
+        rw [h_eq]
+        exact h_subs_yz.1
+
+  theorem subset_transitive' (x y z : U) : (x ⊆ y) → (y ⊂ z) → (x ⊂ z) := by
+    intro h_subs_xy h_subs_yz
+    constructor
+    · apply subseteq_transitive -- [cite: 136]
+      exact h_subs_xy
+      exact h_subs_yz.1
+    · intro h_eq
+      apply h_subs_yz.2
+      apply EqualityOfSubset
+      exact h_subs_yz.1
+      rw [← h_eq]
+      exact h_subs_xy
+
+  theorem subset_transitive'' (x y z : U) : (x ⊂ y) → (y ⊆ z) → (x ⊂ z) := by
+    intro h_subs_xy h_subs_yz
+    constructor
+    · apply subseteq_transitive -- [cite: 136]
+      exact h_subs_xy.1
+      exact h_subs_yz
+    · intro h_eq
+      apply h_subs_xy.2
+      apply EqualityOfSubset
+      exact h_subs_xy.1
+      rw [h_eq]
+      exact h_subs_yz
+
+  noncomputable def isTransitiveSet (x : U) : Prop :=
+    ∀ (y : U), (y ∈ x) → (y ⊂ x) -- [cite: 137]
+
+
   end Extension
 end SetUniverse
 
@@ -105,4 +203,7 @@ export SetUniverse.Extension (
     ExtSet ExtSetReverse ExtSet_wc EqualityOfSubset
     subseteq subseteq_reflexive subseteq_transitive subseteq_antisymmetric
     disjoint
+    subseteq_subset_or_eq subset_subseteq subset_irreflexive
+    subset_asymmetric subset_asymmetric' subset_transitive
+    subset_transitive' subset_transitive'' isTransitiveSet
 )
